322223835fb421b36abce88311b177bc87caef55
LucasLB7 <plucaslambert@gmail.com>
2018-06-09 10:30:25 +0300

    CREATED NEW VIEW:
    Admin is now able to add images to the application
    Have styled the homepage (started styling, still issues)
    Have created relevant relationships with CATEGORIES & TAGS.
    Categories are now displayed for each image
    
    Now need to fix view page for individual photos

--- image_viewer/views.py
+++ image_viewer/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image,Category,Tag
 
-# Create your views here.
+def homePageElements(request):
+    all_images = Image.view_all_pictures()
+    rel_categories = Category.objects.all()
+    date = dt.date.today()
+    
+    return render(request, 'homepage.html', {"date": date, "all_images": all_images, "rel_categories":rel_categories})
+
+
+
+
+
+def images_by_date(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    # if date == dt.date.today():
+    #     return redirect(news_today)
+    
+    project = Image.pictures_by_date(date)
+    return render(request, 'all_images/archives.html', {"date": date, "project":project})
+
+
+
+
+
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'projects/search.html', {"message":message, "articles": searched_articles})
+    else:
+        message = "Oop, looks like that doesn't exist.."
+        return render(request, 'projects/search.html',{"message":message})
+
+def image(request, image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'all_images/image.html', {"image":image})
+
+# def categories(request):
+#     category = Category.objects.all()
+#     return render(request,'projects/article.html', {"category":category})
--- zoomin_photos/settings.py
+++ zoomin_photos/settings.py
@@ -22,7 +22,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ez&)z&u57m4-o%4_ec&tw9icz+_j+pn@98&+jztjkmc+)@cq=a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap3'
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'zoomin_photos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio',
+        'NAME': 'zoominphotos',
         'USER': 'd4rkkn1t3',
         'PASSWORD': 'psql',
     }
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -124,3 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
