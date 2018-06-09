16df0f0a6fbe1a7dfe0696ae94c43f067db52822
LucasLB7 <plucaslambert@gmail.com>
2018-06-09 10:25:04 +0300

    CREATED NEW VIEW:
    Admin is now able to add images to the application
    Have styled the homepage (started styling, still issues)
    Have created relevant relationships with CATEGORIES & TAGS.
    Categories are now displayed for each image
    
    Now need to fix view page for individual photos

--- image_viewer/admin.py
+++ image_viewer/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Editor, Image, Category, Tag
 
-# Register your models here.
+
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category','tag')
+
+
+
+admin.site.register(Editor)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+
+
+
--- image_viewer/models.py
+++ image_viewer/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    nick_name = models.CharField(max_length = 20, blank = True)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
+    editor_photo = models.ImageField(upload_to = 'editors/')
+    
+    def __str__(self):
+        return self.first_name
+    
+    def save_editor(self):
+        self.save()
+
+    def delete_editor(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['first_name']
+
+
+
+class Category(models.Model):
+
+    category_type = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.category_type
+
+
+class Tag(models.Model):
+
+    tags = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.tags
+
+
+class Image(models.Model):
+    title = models.CharField(max_length = 60)
+    description = models.TextField()
+    editor = models.ForeignKey(Editor)
+    category = models.ManyToManyField(Category, related_name='category')
+    tag = models.ManyToManyField(Tag, related_name='tag')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'view_images/')
+
+
+    @classmethod
+    def search_pictures(cls, search_term):
+        result = cls.objects.filter(title__icontains = search_term , category__icontains = search_term , pub_date__date = search_term, tag__icontains = search_term)
+        return results
+    # @classmethod
+    # def search_by_category(cls, search_term):
+    #     result = cls.objects.filter(category__icontains = search_term)
+    #     return results
+    # @classmethod
+    # def search_by_date(cls, search_term):
+    #     result = cls.objects.filter(pub_date__date = search_term)
+    #     return results
+
+
+    @classmethod
+    def view_all_pictures(cls):
+        results = cls.objects.filter()
+        return results
+    
+    @classmethod
+    def pictures_by_date(cls,date):
+        results = cls.objects.filter(pub_date__date = date)
+        return results
+
+
+
+
