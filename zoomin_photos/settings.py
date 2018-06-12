cf1b433ab0ad26f6d1f3258886cd5fd9bbe74c9a
LucasLB7 <plucaslambert@gmail.com>
2018-06-12 18:29:30 +0300

    fixing a filly-bug

--- zoomin_photos/settings.py
+++ zoomin_photos/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 from decouple import config
 import django
-import django-heroku
+import django_heroku
 django_heroku.settings(locals(), staticfiles=False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
